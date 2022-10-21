820c3dbb22eaa2287d682d26f576c2a6403891f6
IdoNirTheGreat <Idonir777@gmail.com>
2022-10-21 21:26:42 +0300

    Added weekday to sensor transmission data.

--- sensor_client.py
+++ sensor_client.py
@@ -18,15 +18,49 @@ if __name__ == "__main__":
 
         # Get time data
         t = localtime()
+        wday = ""
+        if t.tm_wday == 0:
+            wday = "Mon"
+        elif t.tm_wday == 1:
+            wday = "Tue"
+        elif t.tm_wday == 2:
+            wday = "Wed"
+        elif t.tm_wday == 3:
+            wday = "Thu"
+        elif t.tm_wday == 4:
+            wday = "Fri"
+        elif t.tm_wday == 5:
+            wday = "Sat"
+        elif t.tm_wday == 6:
+            wday = "Sun"
+
         tstamp = f"{t.tm_mday:02d}/{t.tm_mon:02d}/{t.tm_year} {t.tm_hour:02d}:{t.tm_min:02d}:{t.tm_sec:02d}" # Only on python
-        # tstamp = f"{t[2]:02d}/{t[1]:02d}/{t[0]}, {t[3]:02d}:{t[4]:02d}:{t[5]:02d}" # Only on Micropython
+        
+        # # Only on Micropython
+        # wday = ""
+        # if t[6] == 0:
+        #     wday = "Mon"
+        # elif t[6] == 1:
+        #     wday = "Tue"
+        # elif t[6] == 2:
+        #     wday = "Wed"
+        # elif t[6] == 3:
+        #     wday = "Thu"
+        # elif t[6] == 4:
+        #     wday = "Fri"
+        # elif t[6] == 5:
+        #     wday = "Sat"
+        # elif t[6] == 6:
+        #     wday = "Sun"
+        # tstamp = f"{t[2]:02d}/{t[1]:02d}/{t[0]}, {t[3]:02d}:{t[4]:02d}:{t[5]:02d}" 
 
         # Collect data
-        data_dict = {"S.N.": SENSOR_NO,
-                "Location": LOCATION,
-                "Time": tstamp,
-                "Entrances": entrances,
-                "Exits": exits}
+        data_dict = {   "S.N.": SENSOR_NO,
+                        "Location": LOCATION,
+                        "Weekday": wday,
+                        "Time": tstamp,
+                        "Entrances": entrances,
+                        "Exits": exits}
         print(data_dict)
 
         # Send data
