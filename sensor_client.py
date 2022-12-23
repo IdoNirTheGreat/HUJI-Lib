deb7ecd086b18b1c92b47824c5d45f2d5907bf3e
IdoNirTheGreat <Idonir777@gmail.com>
2022-12-23 15:25:46 +0200

    Sensor successfully feeds data to server.

--- sensor_client.py
+++ sensor_client.py
@@ -1,85 +1,84 @@
-# This code SHOULD be simulated on Micropython
-
-from requests import post # Should be changed to urequests on Micropython
+import network
+from urequests import post
 from time import sleep, localtime
 
-SERVER_ADDR = "127.0.0.1"
+SERVER_ADDR = "192.168.1.215"
 PORT = 80
 SENSOR_NO = 1
-LOCATION = "Harman Science Library - Floor -1"
+LOCATION = "Harman Science Library - Floor 2 (Quiet)"
 SLEEP_INTERVAL = 10
+WIFI_SSID = "Nir 2"
+WIFI_PD = "Pn0547436227"
 
-if __name__ == "__main__":
-    # Get data from sensors
-    entrances = 30
-    exits = 20
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(WIFI_SSID, WIFI_PD)
 
-    while(True):
+prnt_count = 0
+while not station.isconnected():
+    if prnt_count > 3:
+        prnt_count = 0
+    print("Connecting", end='')
+    print(prnt_count * '.', end='\r')
+    prnt_count += 1
+    sleep(.25)
 
-        # Get time data
-        t = localtime()
-        wday = ""
-        if t.tm_wday == 0:
-            wday = "Mon"
-        elif t.tm_wday == 1:
-            wday = "Tue"
-        elif t.tm_wday == 2:
-            wday = "Wed"
-        elif t.tm_wday == 3:
-            wday = "Thu"
-        elif t.tm_wday == 4:
-            wday = "Fri"
-        elif t.tm_wday == 5:
-            wday = "Sat"
-        elif t.tm_wday == 6:
-            wday = "Sun"
+print("Connected to WiFi.")
 
-        # Only on python
-        dstamp = f"{t.tm_mday:02d}/{t.tm_mon:02d}/{t.tm_year}"  
-        tstamp = f"{t.tm_hour:02d}:{t.tm_min:02d}"
+# Get data from sensors
+entrances = 30
+exits = 20
 
-        # # Only on Micropython
-        # wday = ""
-        # if t[6] == 0:
-        #     wday = "Mon"
-        # elif t[6] == 1:
-        #     wday = "Tue"
-        # elif t[6] == 2:
-        #     wday = "Wed"
-        # elif t[6] == 3:
-        #     wday = "Thu"
-        # elif t[6] == 4:
-        #     wday = "Fri"
-        # elif t[6] == 5:
-        #     wday = "Sat"
-        # elif t[6] == 6:
-        #     wday = "Sun"
-        # dstamp = f"{t[2]:02d}/{t[1]:02d}/{t[0]}"
-        # tstamp = f"{t[3]:02d}:{t[4]:02d}"
+while(True):
+    # Only on Micropython
+    t = localtime()
+    wday = ""
+    if t[6] == 0:
+        wday = "Mon"
+    elif t[6] == 1:
+        wday = "Tue"
+    elif t[6] == 2:
+        wday = "Wed"
+    elif t[6] == 3:
+        wday = "Thu"
+    elif t[6] == 4:
+        wday = "Fri"
+    elif t[6] == 5:
+        wday = "Sat"
+    elif t[6] == 6:
+        wday = "Sun"
+    dstamp = f"{t[2]:02d}/{t[1]:02d}/{t[0]}"
+    tstamp = f"{t[3]:02d}:{t[4]:02d}"
 
-        # Collect data:
-        wday = "Tue" # debugging only
-        tstamp = "11:"+tstamp.split(":")[1] # debugging only
-        data_dict = {   "S.N.": SENSOR_NO,
-                        "Location": LOCATION,
-                        "Weekday": wday,
-                        "Date": dstamp,
-                        "Time": tstamp,
-                        "Entrances": entrances,
-                        "Exits": exits}
-        print(data_dict)
+    # Collect data:
+    wday = "Tue" # debugging only
+    tstamp = "11:"+tstamp.split(":")[1]
+    data_dict = {}
+    data_dict["S.N."] = SENSOR_NO
+    data_dict["Location"] = LOCATION
+    data_dict["Weekday"] = wday
+    data_dict["Date"] = dstamp
+    data_dict["Time"] = tstamp
+    data_dict["Entrances"] = entrances
+    data_dict["Exits"] = exits
+    
+    print(data_dict)
 
-        # Send data
-        try:
-            post("http://"+SERVER_ADDR, data=str(data_dict))
-            print("Sent successfully.")
-            # Make green LED light up or something            
+    # Send data
+    try:
+        post("http://"+SERVER_ADDR, data=str(data_dict))
+        print("Sent successfully.")
+        # Make green LED light up or something            
 
-        except Exception as e:
-            # Connection Failed:
-            print("Connection failed.")
-            print("Exception raised: "+str(e))
-            # Make red LED light up or something
+    except Exception as e:
+        # Connection Failed:
+        print("Connection failed.")
+        print("Exception raised: "+str(e))
+        # Make red LED light up or something
 
-        sleep(SLEEP_INTERVAL)
-        entrances += 1
+    sleep(SLEEP_INTERVAL)
+    entrances += 1
+    
+network.WLAN.disconnect()
+station.active(False)
+print("Disconnected from WiFi.")
