03c3a4482338d5f9670a245c0efdb3687c7266ef
IdoNirTheGreat <Idonir777@gmail.com>
2022-12-24 16:58:43 +0200

    Reuploaded updated files.

--- sensor.py
+++ sensor.py
@@ -7,52 +7,82 @@ SERVER_ADDR = "192.168.1.215"
 PORT = 80
 SENSOR_NO = 1
 LOCATION = "Harman Science Library - Floor 2 (Quiet)"
-TRNSMT_INTERVAL = 10
+TRNSMT_INTERVAL = 30
 WIFI_SSID = "Nir 2"
 WIFI_PD = "Pn0547436227"
 LAN_TIMEOUT = 15 # In seconds
+MOTION_ON = 0
+MOTION_OFF = 1
+MOTION_TIMEOUT = 1 # The timout duration to cancel an entrance or exit if only one sensor was activated.
 
-# Pin Declaration:
+# Components' Declaration:
 led_lan_conn = Pin(13, Pin.OUT) # Lights if successfully connected to LAN, blinks if trying to connect, off if isn"t connected.
 blue = Pin(12, Pin.OUT)
 led_server_success = Pin(14, Pin.OUT) # Successfully connected to server.
 led_server_failure = Pin(27, Pin.OUT) # Failed connecting to server.
-thermo = Pin(2, Pin.IN)
-sound = Pin(4, Pin.IN)
-conn_loading = .5
+# thermo = Pin(2, Pin.IN)
+# sound = Pin(4, Pin.IN)
+pe_sensor_L = Pin(2, Pin.IN)
+pe_sensor_R = Pin(4, Pin.IN)
 
-# WiFi Declaration:
-station = network.WLAN(network.STA_IF)
-station.active(True)
-station.connect(WIFI_SSID, WIFI_PD)
+def check_enter(sensor_L, sensor_R, previous_L, previous_R):
+    if sensor_R.value() != previous_R and sensor_R.value() == MOTION_ON:
+        print(f"Motion!: sensor_R = {sensor_R.value()}, previous_R = {previous_R}")
+        start = time()
+        while time() - start <= MOTION_TIMEOUT:
+            # print(f"Motion!: sensor_R = {sensor_R.value()}, sensor_L = {sensor_L.value()}")
+            if sensor_R.value() == MOTION_OFF and sensor_L.value() == MOTION_ON:
+                print("Entrance!")
+                return 1
+    return 0
 
-# Connect to LAN:
-start = time()
-conn_loading = 0
-while not station.isconnected() and not time()-start > LAN_TIMEOUT:
-    if led_lan_conn.value():
-        led_lan_conn.value(0)
-    else:
-        led_lan_conn.value(1)
-    print(f"\r({time()-start}s) Connecting{((time()-start) % 4) * '.'}   ", end='')
-    sleep(.25)
+def check_exit(sensor_L, sensor_R, previous_L, previous_R) -> int:
+    if sensor_L.value() != previous_L and sensor_L.value() == MOTION_ON:
+        print(f"Motion!: sensor_L = {sensor_L.value()}, previous_L = {previous_L}")
+        start = time()
+        while time() - start <= MOTION_TIMEOUT:
+            # print(f"Motion!: sensor_R = {sensor_R.value()}, sensor_L = {sensor_L.value()}")
+            if sensor_L.value() == MOTION_OFF and sensor_R.value() == MOTION_ON:
+                print("Exit!")
+                return 1
+    return 0
 
-# LAN timeout:
-if time()-start > LAN_TIMEOUT:
-    station.active(False)
-    print("Disconnected from WiFi.")
-    raise Exception(f"LAN_TIMEOUT_EXCEPTION ({LAN_TIMEOUT}s)")
+if __name__ == '__main__':
+    # WiFi Declaration:
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    station.connect(WIFI_SSID, WIFI_PD)
 
-while(True):
+    # Connect to LAN:
+    start = time()
+    while not station.isconnected() and not time()-start > LAN_TIMEOUT:
+        if led_lan_conn.value():
+            led_lan_conn.value(0)
+        else:
+            led_lan_conn.value(1)
+        print(f"\r({time()-start}s) Connecting{((time()-start) % 4) * '.'}   ", end='')
+        sleep(.25)
+
+    # LAN timeout:
+    if time()-start > LAN_TIMEOUT:
+        station.active(False)
+        print("Disconnected from WiFi.")
+        raise Exception(f"LAN_TIMEOUT_EXCEPTION ({LAN_TIMEOUT}s)")
+
     print("\nConnected to WiFi.")
     if not led_lan_conn.value(): # Make LAN LED light solid
         led_lan_conn.value(1)
 
-    # Get data from sensors
-    entrances = 30
-    exits = 20
-
-    while(True):
+    # Add sensor's constant data:
+    data_dict = {}
+    data_dict["S.N."] = SENSOR_NO
+    data_dict["Location"] = LOCATION
+    entrances, exits = 0, 0
+    pe_previous_R, pe_previous_L = MOTION_OFF, MOTION_OFF
+    
+    # Main measuring loop:
+    while(True):              
+        # Get timestamp:
         t = localtime()
         wday = ""
         if t[6] == 0:
@@ -71,22 +101,30 @@ while(True):
             wday = "Sun"
         dstamp = f"{t[2]:02d}/{t[1]:02d}/{t[0]}"
         tstamp = f"{t[3]:02d}:{t[4]:02d}"
-
-        # Collect data:
         wday = "Sun" # debugging only
-        tstamp = "8:"+tstamp.split(":")[1]
-        data_dict = {}
-        data_dict["S.N."] = SENSOR_NO
-        data_dict["Location"] = LOCATION
+        tstamp = "8:"+tstamp.split(":")[1] # debugging only
         data_dict["Weekday"] = wday
         data_dict["Date"] = dstamp
         data_dict["Time"] = tstamp
+
+        # Measure entrances and exits:
+        start_msrmnt = time()
+        print(f"Started Measurement at {start_msrmnt}.")
+        while time() - start_msrmnt <= TRNSMT_INTERVAL:
+            # print(pe_sensor_L.value(), pe_sensor_R.value())
+            entrances += check_enter(pe_sensor_L, pe_sensor_R, pe_previous_L, pe_previous_R)
+            exits += check_exit(pe_sensor_L, pe_sensor_R, pe_previous_L, pe_previous_R)
+            pe_previous_L = pe_sensor_L.value()
+            pe_previous_R = pe_sensor_R.value()
+        print("Ended Measurement.")
+
         data_dict["Entrances"] = entrances
         data_dict["Exits"] = exits
         
         print(data_dict)
 
-        # Send data
+        # Send data to server:
+        print("Sending to server:")
         try:
             post("http://"+SERVER_ADDR, data=str(data_dict))
             print("Sent successfully.")
@@ -98,5 +136,4 @@ while(True):
             print("Exception raised: "+str(e))
             led_server_failure.value(1)
 
-        sleep(TRNSMT_INTERVAL)
-        entrances += 1
+        print("Finished Iteration.\n\n")
