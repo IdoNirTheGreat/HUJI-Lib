b9aaa735fca84cdb1743a22718b55378a584d830
IdoNirTheGreat <Idonir777@gmail.com>
2023-01-28 14:23:31 +0200

    More updates to 'sensor_ver2.py':
    - Created a transmission function, yet to be tested.
    
    - Created an __str__ function which returns all data in sensor.

--- sensor_ver2.py
+++ sensor_ver2.py
@@ -34,8 +34,10 @@ class Sensor:
             functions for the sensor to do.
         """
         # Data Attributes:
-        self.serial_num = SENSOR_NO
-        self.location = LOCATION
+        self.transmission = {}
+        self.transmission["S.N."] = SENSOR_NO
+        self.transmission["Location"] = LOCATION
+        self.update_time()
 
         # WiFi Attirbutes:
         self.station = network.WLAN(network.STA_IF)
@@ -47,6 +49,9 @@ class Sensor:
         self.red_led = Pin(RED_LED_PIN, Pin.OUT)
         self.motion_L = Pin(MOTION_L_PIN, Pin.IN)
         self.motion_R = Pin(MOTION_R_PIN, Pin.IN)
+
+
+    # Connectibility Functions:
 
     def connect(self) -> bool:
         """
@@ -109,8 +114,78 @@ class Sensor:
                     "Exception: {e}")
             return False
 
+    def transmit(self) -> bool:
+        """
+            Sends the transmission dict to the server.
+            Lights green LED and returns True if succeeded,
+            Lights red LED and returns False else.
+        """
+
+        try:
+            post("http://"+SERVER_ADDR, data=str(self.transmission))
+        
+        except Exception as e:
+            print(f"Transmission failed.\n Exception: {e}")
+            self.red_led.value(1)
+            return False
+        
+        print(f"Transmission sent at {self.transmission['Date']}, {self.transmission['Time']}")
+        self.green_led.value(1)
+        return True
+
+    # Data Proccessing Functions:
+
+    def __str__(self) -> str:
+        """
+            Return value of all attributes of self as a string.
+        """
+
+        return  f"\n### Sensor {self.transmission['S.N.']} - Beginning of Report ###\n" + \
+                f"Sensor Location: {self.transmission['Location']}\n" + \
+                f"Connection Status: {self.station.isconnected()}\n\n" + \
+                "Dictionary Values: \n" + "".join([f"{key}: {value}\n" for key, value in zip(self.transmission.keys(), self.transmission.values())]) + \
+                "\nComponents Values: \n" + \
+                    f"Yellow LED: {self.yellow_led.value()}\n" + \
+                    f"Blue LED: {self.blue_led.value()}\n" + \
+                    f"Green LED: {self.green_led.value()}\n" + \
+                    f"Red LED: {self.red_led.value()}\n" + \
+                    f"Motion Sensor (L): {self.motion_L.value()}\n" + \
+                    f"Motion Sensor (R): {self.motion_R.value()}\n" + \
+                "\n### End of Report ###\n"
+
+    def update_time(self) -> None:
+        """
+            Update Weekday, date and time in self.transmission.
+        """
+        
+        # Get timestamp:
+        t = localtime()
+        wday = ""
+        if t[6] == 0:
+            wday = "Mon"
+        elif t[6] == 1:
+            wday = "Tue"
+        elif t[6] == 2:
+            wday = "Wed"
+        elif t[6] == 3:
+            wday = "Thu"
+        elif t[6] == 4:
+            wday = "Fri"
+        elif t[6] == 5:
+            wday = "Sat"
+        elif t[6] == 6:
+            wday = "Sun"
+        dstamp = f"{t[2]:02d}/{t[1]:02d}/{t[0]}"
+        tstamp = f"{t[3]:02d}:{t[4]:02d}"
+        wday = "Sun" # debugging only
+        tstamp = "8:"+tstamp.split(":")[1] # debugging only
+        self.transmission["Weekday"] = wday
+        self.transmission["Date"] = dstamp
+        self.transmission["Time"] = tstamp
+
 if __name__ == '__main__':
     sensor = Sensor()
     sensor.connect()
+    print(sensor)
     sleep(3)
     sensor.disconnect()
